f3353b8a3ec9b91e1df7dc0d3cdcd6ee85d67c60
hramykalu <90501314+hramykalu@users.noreply.github.com>
2022-02-27 14:59:06 +0300

    Update SQL_DDL_1.sql

--- SQL_DDL_1.sql
+++ SQL_DDL_1.sql
@@ -1,10 +1,10 @@
- -- 1)Создать таблицу employees (id. serial,  primary key; employee_name. Varchar(50), not null);
+ -- 1)Г‘Г®Г§Г¤Г ГІГј ГІГ ГЎГ«ГЁГ¶Гі employees (id. serial,  primary key; employee_name. Varchar(50), not null);
 create table employees (
 id serial primary key,
 employee_name varchar(50) not null
 );
 
--- 2) Наполнить таблицу employee 70 строками;
+-- 2) ГЌГ ГЇГ®Г«Г­ГЁГІГј ГІГ ГЎГ«ГЁГ¶Гі employee 70 Г±ГІГ°Г®ГЄГ Г¬ГЁ;
 insert into employees(id, employee_name)
 values 
 (default, 'sarah'),
@@ -79,13 +79,13 @@ values
 (default, 'sarah')
 ;
 
--- 3) Создать таблицу salary(id. Serial  primary key; monthly_salary. Int, not null);
+-- 3) Г‘Г®Г§Г¤Г ГІГј ГІГ ГЎГ«ГЁГ¶Гі salary(id. Serial  primary key; monthly_salary. Int, not null);
 create table salary(
 id serial primary key, 
 mounthly_salary int not null
 );
 
--- 4) Наполнить таблицу salary 15 строками;
+-- 4) ГЌГ ГЇГ®Г«Г­ГЁГІГј ГІГ ГЎГ«ГЁГ¶Гі salary 15 Г±ГІГ°Г®ГЄГ Г¬ГЁ;
  insert into salary(id, mounthly_salary)
  values (default, 1000),
  (default, 1100),
@@ -101,16 +101,16 @@ mounthly_salary int not null
  (default, 2100),
  (default, 2200),
  (default, 2300),
- (default, 2400),
- (default, 2500);
+ (default, 2400)
+;
 
---5) Создать таблицу employee_salary (id. Serial  primary key; employee_id. Int, not null, unique; salary_id. Int, not null)
+--5) Г‘Г®Г§Г¤Г ГІГј ГІГ ГЎГ«ГЁГ¶Гі employee_salary (id. Serial  primary key; employee_id. Int, not null, unique; salary_id. Int, not null)
 create table employee_salary (
 id serial primary key,
 employee_id int not null unique,
 salary_id int not null);
 
--- 6) Наполнить таблицу employee_salary 40 строками: в 10 строк из 40 вставить несуществующие employee_id
+-- 6) ГЌГ ГЇГ®Г«Г­ГЁГІГј ГІГ ГЎГ«ГЁГ¶Гі employee_salary 40 Г±ГІГ°Г®ГЄГ Г¬ГЁ: Гў 10 Г±ГІГ°Г®ГЄ ГЁГ§ 40 ГўГ±ГІГ ГўГЁГІГј Г­ГҐГ±ГіГ№ГҐГ±ГІГўГіГѕГ№ГЁГҐ employee_id
 insert into employee_salary 
 values (default, 3, 7),
 (default, 1, 4),
@@ -153,17 +153,17 @@ values (default, 3, 7),
 (default, 260, 4),
 (default, 163, 1);
 
---7) Создать таблицу roles( id. Serial  primary key; role_name. int, not null, unique);
+--7) Г‘Г®Г§Г¤Г ГІГј ГІГ ГЎГ«ГЁГ¶Гі roles( id. Serial  primary key; role_name. int, not null, unique);
 create table roles (
 id serial primary key, 
 role_name int not null unique);
 
---8) Поменять тип столба role_name с int на varchar(30)
+--8) ГЏГ®Г¬ГҐГ­ГїГІГј ГІГЁГЇ Г±ГІГ®Г«ГЎГ  role_name Г± int Г­Г  varchar(30)
 alter table roles
 alter column  role_name type varchar(30) 
 using role_name::varchar(30);
 
---9) Наполнить таблицу roles 20 строками:
+--9) ГЌГ ГЇГ®Г«Г­ГЁГІГј ГІГ ГЎГ«ГЁГ¶Гі roles 20 Г±ГІГ°Г®ГЄГ Г¬ГЁ:
 insert into roles (id, role_name)
 values (default,'Junior Python developer'),
 (default, 'Middle Python developer'),
@@ -186,7 +186,7 @@ values (default,'Junior Python developer'),
 (default, 'Middle Automation QA engineer'),
 (default, 'Senior Automation QA engineer');
 
--- 10) Создать таблицу roles_employee (id. Serial primary key; employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id); role_id. Int, not null (внешний ключ для таблицы roles, поле id) )
+-- 10) Г‘Г®Г§Г¤Г ГІГј ГІГ ГЎГ«ГЁГ¶Гі roles_employee (id. Serial primary key; employee_id. Int, not null, unique (ГўГ­ГҐГёГ­ГЁГ© ГЄГ«ГѕГ· Г¤Г«Гї ГІГ ГЎГ«ГЁГ¶Г» employees, ГЇГ®Г«ГҐ id); role_id. Int, not null (ГўГ­ГҐГёГ­ГЁГ© ГЄГ«ГѕГ· Г¤Г«Гї ГІГ ГЎГ«ГЁГ¶Г» roles, ГЇГ®Г«ГҐ id) )
 create table roles_employee(
 id serial primary key,
 employee_id int not null unique,
@@ -197,7 +197,7 @@ foreign key (role_id)
 references roles_employee (id)
 );
 
--- 11) Наполнить таблицу roles_employee 40 строками:
+-- 11) ГЌГ ГЇГ®Г«Г­ГЁГІГј ГІГ ГЎГ«ГЁГ¶Гі roles_employee 40 Г±ГІГ°Г®ГЄГ Г¬ГЁ:
 insert into roles_employee(id, employee_id, role_id)
 values 
  (default, 7,2),
